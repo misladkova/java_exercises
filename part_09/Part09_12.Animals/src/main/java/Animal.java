92177cd55d584aa2a671429c3baa10f4ebb42eb2
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-10-10 00:50:04 +0200

    add file implement abstract class

---
+++ part_09/Part09_12.Animals/src/main/java/Animal.java
@@ -0,0 +1,20 @@
+public abstract class Animal {
+
+    private String animalName;
+
+    public Animal(String name){
+        this.animalName = name;
+    }
+
+    public void eat(){
+        System.out.println(animalName+" eats");
+    }
+
+    public void sleep(){
+        System.out.println(animalName+" sleeps");
+    }
+
+    public String getName(){
+        return animalName;
+    }
+}
