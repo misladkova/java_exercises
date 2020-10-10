3d0eff962967302fa92dc5660b4500f94d87f50a
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-10-10 18:01:48 +0200

    add file

---
+++ part_10/Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AverageOfSelectedNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Input numbers, type \"end\" to stop.");
+
+
+
+    }
+}
