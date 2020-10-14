818c0469af7972ccb53e3157f0d16e1883909e79
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-10-14 23:24:42 +0200

    cleaning

--- part_11/Part11_10.FlightControl/src/main/java/FlightControl/Main.java
+++ part_11/Part11_10.FlightControl/src/main/java/FlightControl/Main.java
@@ -12,7 +12,11 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         UI ui = new UI(scan);
 
+        System.out.println("Airport Asset Control");
+        System.out.println();
         ui.airportControl();
+        System.out.println();
+        System.out.println("Flight Control");
         System.out.println();
         ui.flightControl();
     }
