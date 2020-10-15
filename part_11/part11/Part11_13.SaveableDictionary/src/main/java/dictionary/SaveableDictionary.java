81dcd3fcdebfd1509948953c15d3cce428069e4e
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-10-15 23:59:43 +0200

    add method

--- part_11/part11/Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
+++ part_11/part11/Part11_13.SaveableDictionary/src/main/java/dictionary/SaveableDictionary.java
@@ -1,13 +1,19 @@
 package dictionary;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class SaveableDictionary {
 
     private HashMap<String, String> dictionary;
+    private String file;
 
-    public SaveableDictionary() {
+    public SaveableDictionary(String file) {
         this.dictionary = new HashMap<>();
+        this.file = file;
     }
 
     public void add(String words, String translation) {
@@ -24,6 +30,26 @@ public class SaveableDictionary {
             }
         }
         return null;
+    }
+
+    public void delete(String word){
+        dictionary.remove(word);
+        dictionary.entrySet().removeIf(entry -> (word.equals(entry.getValue())));
+    }
+
+    public boolean load(){
+        Scanner reader = null;
+        try {
+            reader = new Scanner(Paths.get(file));
+        } catch (IOException e) {
+            return false;
+        }
+        while(reader.hasNextLine()){
+            String line = reader.nextLine();
+            String[] parts = line.split(":");
+            dictionary.putIfAbsent(parts[0], parts[1]);
+        }
+        return true;
     }
 
 }
