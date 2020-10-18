ec0d8ac860e77351df49e4664db0b5d13e08646f
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-10-18 16:37:25 +0200

    add files

---
+++ part_12/Part12_10.MagicSquare/src/main/java/Program.java
@@ -0,0 +1,9 @@
+
+public class Program {
+
+    public static void main(String[] args) {
+        
+        MagicSquareFactory msFactory = new MagicSquareFactory();
+        System.out.println(msFactory.createMagicSquare(5));
+    }
+}
