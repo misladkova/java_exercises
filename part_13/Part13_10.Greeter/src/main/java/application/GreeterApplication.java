93d93f712f0a5adafaac00ccbe56dd4e3d9b80f2
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-10-20 14:20:10 +0200

    change alignment

--- part_13/Part13_10.Greeter/src/main/java/application/GreeterApplication.java
+++ part_13/Part13_10.Greeter/src/main/java/application/GreeterApplication.java
@@ -1,13 +1,16 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -17,14 +20,25 @@ public class GreeterApplication extends Application {
     public void start(Stage stage) throws Exception {
 
         Label label = new Label("Enter your name and start.");
-        PasswordField password = new PasswordField();
+        TextField password = new TextField();
+        Button start = new Button("Start");
+
         GridPane gp = new GridPane();
-        gp.getChildren().addAll(label, password);
+
+        gp.add(label, 0, 0);
+        gp.add(password, 0, 1);
+        gp.add(start, 0, 2);
+        gp.setPrefSize(300, 200);
         gp.setAlignment(Pos.CENTER);
+        GridPane.setHalignment(start, HPos.CENTER);
+        GridPane.setHalignment(label, HPos.CENTER);
+        GridPane.setHalignment(password, HPos.CENTER);
+        gp.setVgap(10);
 
         Label greeting = new Label("Welcome"+password.getText()+"!");
         StackPane sp = new StackPane();
-        sp.getChildren().add(label);
+        sp.getChildren().add(greeting);
+        sp.setAlignment(Pos.CENTER);
 
         Scene scene1 = new Scene(gp);
         Scene scene2 = new Scene(sp);
@@ -34,7 +48,10 @@ public class GreeterApplication extends Application {
     }
 
     public static void main(String[] args) {
+
         System.out.println("Hello world! :3");
+
+        launch(GreeterApplication.class);
     }
 }
 
