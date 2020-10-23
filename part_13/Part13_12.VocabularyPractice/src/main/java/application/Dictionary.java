8718b3c01519946500a03c9e9abf0997dc167f02
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-10-23 06:46:25 +0200

    add class

---
+++ part_13/Part13_12.VocabularyPractice/src/main/java/application/Dictionary.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.*;
+
+public class Dictionary {
+
+    private List<String> words;
+    private Map<String, String> translations;
+
+    public Dictionary(){
+        this.words = new ArrayList<>();
+        this.translations = new HashMap<>();
+    }
+
+    public void addTranslation(String word, String translation){
+        if(!(translations.containsKey(word))){
+            words.add(word);
+        }
+        translations.put(word, translation);
+    }
+
+    public String getTranslation(String word){
+        return translations.get(word);
+    }
+
+    public String getRandomWord(){
+        Random randomWord = new Random();
+        return words.get(randomWord.nextInt(words.size()));
+    }
+
+}
