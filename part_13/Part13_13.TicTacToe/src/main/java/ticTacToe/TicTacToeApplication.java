5cb72f60af1add6478235bd5003196be6d7c654f
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-10-25 19:57:31 +0100

    add methods

--- part_13/Part13_13.TicTacToe/src/main/java/ticTacToe/TicTacToeApplication.java
+++ part_13/Part13_13.TicTacToe/src/main/java/ticTacToe/TicTacToeApplication.java
@@ -2,6 +2,7 @@ package ticTacToe;
 
 
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,28 +16,26 @@ public class TicTacToeApplication extends Application {
 
     private String player;
     private boolean boo;
+    private GridPane gp;
 
-    public TicTacToeApplication(){
+    public TicTacToeApplication() {
         this.player = "X";
         this.boo = false;
+        this.gp = new GridPane();
     }
 
     @Override
     public void start(Stage stage) throws Exception {
 
-        Label turn = new Label("Turn: "+player());
-        //Button b = new Button(" ");
-        //b.setFont(Font.font("Monospaced", 40));
+        Label turn = new Label("Turn: " + player());
 
         BorderPane bp = new BorderPane();
         bp.setTop(turn);
 
-
-        GridPane gp = new GridPane();
         gp.setVgap(10);
         gp.setHgap(10);
-        //gp.add(new Button(" "), 0, 0);
-        for(int row = 0; row<3; row++) {
+
+        for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
                 Button button = new Button(" ");
                 button.setFont(Font.font("Monospaced", 30));
@@ -46,7 +45,15 @@ public class TicTacToeApplication extends Application {
                         button.setText(player());
                         boo = !boo;
                     }
-                    turn.setText("Turn: "+player());
+                    boolean isOver = endOfGame();
+                    if (isOver == true) {
+                        turn.setText("The end!");
+                        for (Node b : gp.getChildren()) {
+                            b.setDisable(true);
+                        }
+                    } else {
+                        turn.setText("Turn: " + player());
+                    }
                 });
             }
         }
@@ -58,17 +65,100 @@ public class TicTacToeApplication extends Application {
         stage.show();
     }
 
-    public boolean whichPlayer(){
+    public boolean whichPlayer() {
         return boo;
     }
 
-    public String player(){
-        if(boo==false){
+    public String player() {
+        if (boo == false) {
             player = "X";
-            return player;
+        } else {
+            player = "O";
         }
-        player = "O";
         return player;
+    }
+
+    public boolean checkRow(int row) {
+        Button a = (Button) getNode(row, 0);
+        Button b = (Button) getNode(row, 1);
+        Button c = (Button) getNode(row, 2);
+        if ((a.getText().equals(b.getText()) && b.getText().equals(c.getText()))&&(!(a.getText().equals(" ")))) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean checkRows() {
+        for (int i = 0; i < 3; i++) {
+            boolean a = checkRow(i);
+            if (a) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkCol(int col) {
+        Button a = (Button) getNode(0, col);
+        Button b = (Button) getNode(1, col);
+        Button c = (Button) getNode(2, col);
+        if ((a.getText().equals(b.getText()) && b.getText().equals(c.getText()))&&(!(a.getText().equals(" ")))) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkColumns() {
+        for (int i = 0; i < 3; i++) {
+            boolean a = checkCol(i);
+            if (a) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkDiagonalR() {
+        Button a = (Button) getNode(0, 2);
+        Button b = (Button) getNode(1, 1);
+        Button c = (Button) getNode(2, 0);
+        if ((a.getText().equals(b.getText()) && b.getText().equals(c.getText()))&&(!(a.getText().equals(" ")))) {
+
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkDiagonalL() {
+        Button a = (Button) getNode(0, 0);
+        Button b = (Button) getNode(1, 1);
+        Button c = (Button) getNode(2, 2);
+        if ((a.getText().equals(b.getText()) && b.getText().equals(c.getText()))&&(!(a.getText().equals(" ")))) {
+
+            return true;
+        }
+        return false;
+    }
+
+    public boolean endOfGame() {
+        boolean a = checkRows();
+        boolean b = checkColumns();
+        boolean c = checkDiagonalR();
+        boolean d = checkDiagonalL();
+        if (a || b || c || d) {
+            return true;
+        }
+        return false;
+    }
+
+    public Node getNode(int x, int y) {
+        for (Node node : gp.getChildren()) {
+            if (GridPane.getColumnIndex(node) == y && GridPane.getRowIndex(node) == x) {
+                return node;
+            }
+        }
+        return new Button(" ");
     }
 
     public static void main(String[] args) {
@@ -77,5 +167,4 @@ public class TicTacToeApplication extends Application {
 
         launch(TicTacToeApplication.class);
     }
-
 }
