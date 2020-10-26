32db7a447c4485ddcbca2b51844e8a676017ec5e
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-10-26 10:38:20 +0100

    add components

--- part_14/part14-Part14_03.SavingsCalculator/part14/Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
+++ part_14/part14-Part14_03.SavingsCalculator/part14/Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
@@ -24,20 +24,21 @@ public class SavingsCalculatorApplication extends Application {
         Label savings = new Label("Monthly savings");
         Label interestRate = new Label("Yearly interest rate");
         Slider firstSlider = new Slider(25, 250, 25);
-        firstSlider.setBlockIncrement(25);
         firstSlider.setShowTickLabels(true);
         firstSlider.setShowTickMarks(true);
         Slider secondSlider = new Slider(0, 10, 0);
-
+        Label savingsValue = new Label(Double.toString(firstSlider.getValue()));
+        Label interestRateValue = new Label(Double.toString(secondSlider.getValue()));
 
         BorderPane slider1 = new BorderPane();
         slider1.setLeft(savings);
         slider1.setCenter(firstSlider);
-
+        slider1.setRight(savingsValue);
 
         BorderPane slider2 = new BorderPane();
         slider2.setLeft(interestRate);
         slider2.setCenter(secondSlider);
+        slider2.setRight(interestRateValue);
 
         VBox vBox = new VBox();
         vBox.getChildren().addAll(slider1, slider2);
@@ -62,7 +63,4 @@ public class SavingsCalculatorApplication extends Application {
     }
 
 }
-//        The first BorderPane (on top) contains the text "Monthly savings" on the left, a slider in the middle
-//        and a text describing the slider on the right. The second BorderPane (below the first one) has the text
-//        "Yearly interest rate" on the left, a slider in the middle and a text describing the slider on the right.
 
