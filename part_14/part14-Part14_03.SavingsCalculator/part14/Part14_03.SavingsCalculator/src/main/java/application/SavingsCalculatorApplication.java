0b13a220d4a31038712e6a4df623c4879e223182
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-10-26 15:35:05 +0100

    add method

--- part_14/part14-Part14_03.SavingsCalculator/part14/Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
+++ part_14/part14-Part14_03.SavingsCalculator/part14/Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
@@ -1,10 +1,13 @@
 package application;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
@@ -13,22 +16,72 @@ import javafx.stage.Stage;
 
 public class SavingsCalculatorApplication extends Application {
 
+    Slider firstSlider = new Slider(25, 250, 25);
+    Slider secondSlider = new Slider(0, 10, 0);
+    Label savingsValue = new Label(Double.toString(firstSlider.getValue()));
+    Label interestRateValue = new Label(Double.toString(secondSlider.getValue()));
+    NumberAxis x = new NumberAxis(0, 30, 1);
+    NumberAxis y = new NumberAxis();
+    LineChart<Number, Number> lineChart = new LineChart<>(x, y);
+
+    public void updateData(double sav, double rate){
+
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
 
-        NumberAxis x = new NumberAxis(0, 30, 1);
-        NumberAxis y = new NumberAxis();
-        LineChart<Number, Number> lineChart = new LineChart<>(x, y);
         lineChart.setTitle("Savings Calculator");
 
         Label savings = new Label("Monthly savings");
         Label interestRate = new Label("Yearly interest rate");
-        Slider firstSlider = new Slider(25, 250, 25);
+        //Slider firstSlider = new Slider(25, 250, 25);
         firstSlider.setShowTickLabels(true);
         firstSlider.setShowTickMarks(true);
-        Slider secondSlider = new Slider(0, 10, 0);
-        Label savingsValue = new Label(Double.toString(firstSlider.getValue()));
-        Label interestRateValue = new Label(Double.toString(secondSlider.getValue()));
+
+        XYChart.Series<Number, Number> data = new XYChart.Series<>();
+
+        firstSlider.valueProperty().addListener((ov, old_val, new_val) -> {
+            double sav = (double) new_val;
+            double rate = secondSlider.getValue();
+            updateData(sav, rate);
+//            savingsValue.setText(String.format("%.1f", new_val));
+//
+//            XYChart.Series<Number, Number> data1 = new XYChart.Series<>();
+//
+//            for (int years = 0; years < 31; years++) {
+//                data1.getData().add(new XYChart.Data<>(years, (years * 12 * firstSlider.getValue())));
+//            }
+////            lineChart.getData().clear();
+//            lineChart.getData().set(0, data1);
+//            //lineChart.getData().add(data1);
+        });
+
+        secondSlider.valueProperty().addListener((ov, old_val, new_val) -> {
+            double sav = firstSlider.getValue();
+            double rate = (double) new_val;
+            updateData(sav, rate);
+//            interestRateValue.setText(String.format("%.1f", new_val));
+
+//            XYChart.Series<Number, Number> data12 = new XYChart.Series<>();
+//
+//            double sum = 0;
+//            for (int years = 0; years < 31; years++) {
+//                double a = ((sum + firstSlider.getValue() * 12) / 100 * secondSlider.getValue());
+//                sum += a + (firstSlider.getValue() * 12);
+//                data12.getData().add(new XYChart.Data<>(years, sum));
+//            }
+//            if(lineChart.getData().size() > 1){
+//                lineChart.getData().set(1, data12);
+//            }else{
+//                lineChart.getData().add(data12);
+//            }
+        });
+
+        for (int years = 0; years < 31; years++) {
+            data.getData().add(new XYChart.Data<>(years, (years * 12 * firstSlider.getValue())));
+        }
+        lineChart.getData().add(data);
 
         BorderPane slider1 = new BorderPane();
         slider1.setLeft(savings);
@@ -52,8 +105,8 @@ public class SavingsCalculatorApplication extends Application {
         Scene scene = new Scene(bp, 650, 400);
         stage.setScene(scene);
         stage.show();
-
     }
+
 
     public static void main(String[] args) {
 
