7e8bd9293bc11093fb8b6082d577700d9efc71c1
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-10-28 01:31:29 +0100

    cleaning

--- part_14/part14-Part14_09.Asteroids/part14/Part14_09.Asteroids/src/main/java/asteroids/DrawPolygon.java
+++ part_14/part14-Part14_09.Asteroids/part14/Part14_09.Asteroids/src/main/java/asteroids/DrawPolygon.java
@@ -63,14 +63,13 @@ public abstract class DrawPolygon {
         if(shape.getTranslateY()>AsteroidsApplication.HEIGHT){
             shape.setTranslateY(shape.getTranslateY()%AsteroidsApplication.HEIGHT);
         }
-        shape.setTranslateX(shape.getTranslateX()+location.getX());
-        shape.setTranslateY(shape.getTranslateY()+location.getY());
     }
 
     public void accelerate(){
         double changeX = Math.cos(Math.toRadians(shape.getRotate()));
         double changeY = Math.sin(Math.toRadians(shape.getRotate()));
-        location = location.add((changeX)*0.005, (changeY)*0.005);
+
+       location = location.add((changeX)*0.005, (changeY)*0.005);
     }
 
     public boolean collide(Polygon s){
